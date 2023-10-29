5d44e6cc0b1a1b8e8fb804ae3a35e0c21738edd6
rriffa <145914122+rriffa@users.noreply.github.com>
2023-10-29 20:58:23 +0700

    Update Latihan24.java

--- src/latihan24/Latihan24.java
+++ src/latihan24/Latihan24.java
@@ -21,28 +21,29 @@ public class Latihan24 {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        
         boolean ulangi = true;
-
-        while (ulangi) {
-            System.out.print("Masukkan Nilai Pertama\t: ");
+        
+        while(ulangi){
+            System.out.println("Masukan nilai pertama\t: ");
             int nilai1 = input.nextInt();
-            System.out.print("Masukkan Nilai Kedua\t: ");
+            System.out.println("Masukan nilai kedua\t: ");
             int nilai2 = input.nextInt();
-
-            if (nilai1 > nilai2) {
-                System.out.println(nilai1 + "lebih besar dari " + nilai2);
-            } else if (nilai1 < nilai2) {
-                System.out.println(nilai1 + "lebih kecil dari" + nilai2);
+            
+            if(nilai1 > nilai2) {
+                System.out.println(nilai1+ "Lebih besar dari" +nilai2);
+            } else if(nilai1 < nilai2) {
+                System.out.println(nilai1+"Lebih kecil dari" +nilai2);
             } else {
-                System.out.println(nilai1 + "sama dengan" + nilai2);
+                System.out.println(nilai1+"sama dengan" +nilai2);
             }
-            System.out.print("ulangi aktifitas? (ya/tidak)");
+            
+            System.out.println("ulangi aktifitas? (ya/tidak)");
             String aktifitas = input.next();
-            if (aktifitas.equals("ya")) {
-                System.out.print("Program akan diulangi");
+            if(aktifitas.equals("ya")) {
+                System.out.println("Program akan diulangi");
             } else if (aktifitas.equals("tidak")) {
-                System.out.print("Program Berhenti Terimakasih");
+                System.out.println("Program berhasil Terimakasih");
                 ulangi = false;
             }
         }
